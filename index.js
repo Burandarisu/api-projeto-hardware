a0629a2b1f80ea99b37678786e17067b911c1ee8
pedrohenriquebrandalisedasilva <web@agenciaade.com.br>
2021-09-26 18:15:56 -0300

    v1.2

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const app = require('./src/app')
-process.env.TZ = 'GMT'
+const app = require("./src/app");
+const aedes = require("aedes")();
+const mqtt = require("mqtt");
+
+const broker = require("net").createServer(aedes.handle);
+const port = 9090;
+
+process.env.TZ = "GMT";
 
 app.listen(8080, () => {
-  console.log('app listening on port 8080')
-})
+  console.log("app listening on port 8080");
+});
+
+broker.listen(port, function () {
+  console.log("server started and listening on port ", port);
+  const client = mqtt.connect("mqtt://localhost:9090");
+  client.on("connect", () => {
+    client.subscribe("motor");
+  });
+  client.on("message", (topic, message) => {
+    if (topic === "motor") {
+      console.log({ message: message.toString() });
+    }
+  });
+});
--- src/controllers/MeasureController.js
+++ src/controllers/MeasureController.js
@@ -1,7 +1,6 @@
 const MeasureCollection = require("../services/firebase/collection/measure");
 
 async function index(req, res) {
-  console.log(new Date(req.query.created_at_begin).getTime());
   const data = await MeasureCollection.get(req.query);
   return res.status(200).send({
     data,
